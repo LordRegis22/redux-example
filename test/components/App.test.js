b201906a255a9aab787497434f2748f1d2eab44f
LordRegis22 <45404541+LordRegis22@users.noreply.github.com>
2021-04-19 19:07:40 -0700

    Create test for basic app render with children

--- test/components/App.test.js
+++ test/components/App.test.js
@@ -4,21 +4,21 @@ import { Provider } from 'react-redux';
 import store from '../../client/store';
 import App from '../../client/components/App';
 import Buttons from '../../client/components/Buttons';
+import Counter from '../../client/components/Counter';
 import Adapter from 'enzyme-adapter-react-16';
 
 //configure({ adapter: new Adapter() });
 
 describe('App component', () => {
-  it('should render App component with class of App and three children', () => {
+  let shallowApp;
+  beforeEach(() => {
     // shallow mount App component
-    const shallowApp = shallow(<App />);
+    shallowApp = shallow(<App />);
+  });
+  it('should render App component with class of App and three children', () => {
     // assert it has class of App
     expect(shallowApp.hasClass('App')).toBe(true);
     // assert it has three children
     expect(shallowApp.children().length).toBe(3);
-    // assert three children are Buttons, Counter and a div for hot reloading
-    console.log(shallowApp.children());
-
-    //expect(shallowApp.dive().find(Buttons)).toBe(true);
   });
 });
