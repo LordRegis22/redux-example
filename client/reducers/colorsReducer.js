eb8c80f2c6224eeea2fde8fdf85b1559711d83de
LordRegis22 <45404541+LordRegis22@users.noreply.github.com>
2021-08-09 23:39:26 -0700

    Create gold button

--- client/reducers/colorsReducer.js
+++ client/reducers/colorsReducer.js
@@ -1,7 +1,7 @@
 import * as actions from '../constants/actionTypes';
 
 const initialState = {
-  colors: ['chartreuse', 'cyan', 'magenta', 'orange'],
+  colors: ['chartreuse', 'cyan', 'magenta', 'orange', 'gold'],
   currentColor: 'chartreuse',
   clickedColors: [],
 };
